52013f0a8643274b521aa0cff9412b307c86c945
happydev0110 <happydev0110@gmail.com>
2024-02-27 13:25:27 -0600

    update

--- src/components/Event/index.js
+++ src/components/Event/index.js
@@ -6,13 +6,16 @@ import Filter from '../../layouts/Filter';
 
 function EventComponent() {
     const [eventId, setEventId] = useState(-1);
-    const [data, setData] = useState([]);
-    const [playList, setEvenList] = useState([]);
+    const [events, setEvents] = useState([]);
+    const [playList, setPlayList] = useState([]);
     const [intervalTime, setIntervalTime] = useState(1);
 
     const [time, setTime] = useState();
     const [team1Idx, setTeam1Idx] = useState(-1);
     const [team2Name, setTeam2Name] = useState('');
+
+    const [team1Score, setTeam1Score] = useState(0);
+    const [team2Score, setTeam2Score] = useState(0);
 
     const [selTblIdx, setSelTblIdx] = useState(0);
     const [table1Score, setTable1Score] = useState(0);
@@ -24,7 +27,7 @@ function EventComponent() {
         // Get Total Event
         try {
             axios.get(URL.EVENT).then((response) => {
-                setData(response.data);
+                setEvents(response.data.events);
             }).catch((err) => {
                 console.log(err);
             });
@@ -45,14 +48,24 @@ function EventComponent() {
                     }
                 ).then((response) => {
                     // console.log(response.data,'data')
-                    setEvenList(response.data);
+                    setPlayList(response.data);
+                    var resList = response.data;
+                    if (team1Idx != -1 && resList.playList) {
+                        resList.plays.forEach((item, index) => {
+                            console.log(item, index, 'event play')
+                            let score1 = 0;
+                            if (item.team.id == resList.boxscore.teams[team1Idx].team.id && item.scoreValue == 3) {
+                                score1 = score1 + item.scoreValue;
+                            }
+                        });
+                    }
                 }).catch((err) => {
                     console.log(err)
                 });
             }, intervalTime * 1000);
             return () => clearInterval(interval);
         } else {
-            setEvenList([])
+            setPlayList([])
         }
     }, [eventId, intervalTime])
 
@@ -64,9 +77,9 @@ function EventComponent() {
                     <Filter
                         label='Event'
                         columns={{ label: "name", value: "id" }}
-                        list={data.events ? data.events : []}
-                        handleChange={(id) => { 
-                            setEventId(id); 
+                        list={events ? events : []}
+                        handleChange={(id) => {
+                            setEventId(id);
                             setTeam1Idx(-1);
                         }}
                     />
@@ -77,8 +90,8 @@ function EventComponent() {
                     <label className="form-label" style={{ float: "left" }}>Team1</label>
                     <select className="form-select form-select-sm"
                         value={team1Idx}
-                        onChange={evt => { 
-                            setTeam1Idx(evt.target.value) 
+                        onChange={evt => {
+                            setTeam1Idx(evt.target.value)
                             setTeam2Name(evt.target.value != -1 && playList.boxscore.teams[(parseInt(evt.target.value) + 1) % 2].team.name)
                         }}
                     >
@@ -110,10 +123,12 @@ function EventComponent() {
                     <h5><b>Score:</b></h5>
                 </div>
                 <div className='col-md-2'>
-                    <p className='p-0'>{team1Idx != -1 && playList.boxscore && playList.boxscore.teams[team1Idx].team.name}</p>
+                    <img src={team1Idx != -1 ? playList.boxscore.teams[team1Idx].team.logo : undefined} style={{ width: 40, height: 40 }} />
+                    <p className='p-0 d-inline-block'>{team1Idx != -1 && playList.boxscore && playList.boxscore.teams[team1Idx].team.name}</p>
                 </div>
                 <div className='col-md-2'>
-                    <p className='p-0'>{team1Idx != -1 && playList.boxscore && playList.boxscore.teams[(parseInt(team1Idx) + 1) % 2].team.name}</p>
+                    <img src={team1Idx != -1 ? playList.boxscore.teams[(parseInt(team1Idx) + 1) % 2].team.logo : undefined} style={{ width: 40, height: 40 }} />
+                    <p className='p-0 d-inline-block'>{team1Idx != -1 && playList.boxscore && playList.boxscore.teams[(parseInt(team1Idx) + 1) % 2].team.name}</p>
                 </div>
             </div>
             <div className='row pb-2'>
@@ -128,8 +143,11 @@ function EventComponent() {
                         <h5 className='d-inline-block'>Table1</h5>
                         <p className='d-inline-block text-danger px-4'>{selTblIdx == 0 && 'Current Table'}</p>
                     </div>
-                    <div className={selTblIdx == 0?'border border-danger':'border'}>
-                        <p className='p-3 d-inline-block'><b>score:</b></p>
+                    <div className={selTblIdx == 0 ? 'border border-danger p-3' : 'border p-3'}>
+                        <p className='d-inline-block'><b>Description:</b></p>
+                        <p className='d-inline-block'>{table1Score}</p><br />
+
+                        <p className='d-inline-block'><b>score:</b></p>
                         <p className='d-inline-block'>{table1Score}</p>
                     </div>
                 </div>
@@ -140,32 +158,11 @@ function EventComponent() {
                         <h5 className='d-inline-block'>Table2</h5>
                         <p className='d-inline-block text-danger px-4'>{selTblIdx == 1 && 'Current Table'}</p>
                     </div>
-                    <div className={selTblIdx == 1?'border border-danger':'border'}>
-                        <p className='p-3 d-inline-block'><b>score:</b></p>
-                        <p className='d-inline-block'>{table1Score}</p>
-                    </div>
-                </div>
-            </div>
-            <div className='row py-3'>
-                <div className='col-md-12'>
-                    <div className='float-left d-inline-block'>
-                        <h5 className='d-inline-block'>Table3</h5>
-                        <p className='d-inline-block text-danger px-4'>{selTblIdx == 2 && 'Current Table'}</p>
-                    </div>
-                    <div className={selTblIdx == 2?'border border-danger':'border'}>
-                        <p className='p-3 d-inline-block'><b>score:</b></p>
-                        <p className='d-inline-block'>{table1Score}</p>
-                    </div>
-                </div>
-            </div>
-            <div className='row py-3'>
-                <div className='col-md-12'>
-                    <div className='float-left d-inline-block'>
-                        <h5 className='d-inline-block'>Table4</h5>
-                        <p className='d-inline-block text-danger px-4'>{selTblIdx == 3 && 'Current Table'}</p>
-                    </div>
-                    <div className={selTblIdx == 3?'border border-danger':'border'}>
-                        <p className='p-3 d-inline-block'><b>score:</b></p>
+                    <div className={selTblIdx == 1 ? 'border border-danger p-3' : 'border p-3'}>
+                        <p className='d-inline-block'><b>Description:</b></p>
+                        <p className='d-inline-block'>{table1Score}</p><br />
+
+                        <p className='d-inline-block'><b>score:</b></p>
                         <p className='d-inline-block'>{table1Score}</p>
                     </div>
                 </div>
