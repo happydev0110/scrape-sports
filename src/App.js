4a8a3a73335733c766f5ddfcce514d6a928637f2
Happy Dev <happydev0110@gmail.com>
2024-04-17 23:22:40 -0500

    add access page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 // import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 // import Dashboard from './components/Dashboard';
+import React, { useState } from 'react';
 import Event from './components/Event';
 
 import 'bootstrap/dist/css/bootstrap.css'
 import './assets/App.css';
 
+const PASSWORD = 'BallHog'
+
 function App() {
+  const [access, setAccess] = useState(false);
+  const [pass, setPass] = useState();
+
+  const getAccess = () => {
+    let status = false
+    if(pass === PASSWORD && access){
+      status = true
+    }
+
+    return status
+  }
+
+  console.log(access,'access')
   return (
     <>
       <div className="p-2 bg-primary text-white text-left">
@@ -26,8 +42,33 @@ function App() {
       </nav> */}
       <div className='container'>
         {/* <Dashboard /> */}
-        
-        <Event />
+        {
+          getAccess() ?
+            <Event /> :
+            <div className='my-3'>
+              <div className='row'>
+                <div className='col-12 text-center'>
+                  {/* <h1>This is Security Part.</h1> */}
+                  <input
+                    className='col-12'
+                    value={pass}
+                    onChange={(evt) => {
+                      setPass(evt.target.value)
+                      setAccess(false)
+                    }}
+                  />
+                  <button
+                    className='btn btn-primary mt-3'
+                    onClick={() =>{
+                      setAccess(true)
+                    }}
+                  >
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </div>
+        }
       </div>
     </>
   );
